4144469987363b48a3cb11c13a80b8a7a14b9262
LAPTOP-GB155NQA\yeari <yearinan@gmail.com>
2021-01-19 18:11:21 +0900

    방 데이터 클래스 RoomVO추가

---
+++ teami/src/main/java/org/teami/domain/RoomVO.java
@@ -0,0 +1,13 @@
+package org.teami.domain;
+
+import lombok.Data;
+
+@Data
+public class RoomVO {
+	
+	private String room_code;
+	private String room_name;
+	
+//	private List<MemberVO> room_member;
+	
+}
