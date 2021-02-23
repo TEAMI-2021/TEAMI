eb7928812b0ca39bc714c2047a09d17eb4baa94e
sg-kmj <2018132081@g.shingu.ac.kr>
2021-02-23 20:30:46 +0900

    404 에러 수정

--- teami/src/main/java/org/teami/controller/BoardController.java
+++ teami/src/main/java/org/teami/controller/BoardController.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import org.teami.domain.BoardReadVO;
@@ -169,6 +170,7 @@ public class BoardController{
 	
 	
 	@PostMapping("/uploadAjaxAction")
+	@ResponseBody
 	public void uploadAjaxPost(MultipartFile[] uploadFile) {
 		
 		log.info("update ajax post.....................");
